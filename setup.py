a4010baf8aaaa7de5dbc536013f56c1b98c778a0
mkaanarici <45287457+mkaanarici@users.noreply.github.com>
2023-03-22 10:43:28 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+setup(
+    name='Paragon',
+    packages=find_packages(include=['Paragon']),
+    version='0.1.0',
+    description='Network Inference Lib',
+    author='M Kaan ARICI, Nurcan Tuncbag',
+    license='MIT',
+    url='https://github.com/metunetlab/Paragon',
+    python_requires='>=3.6',
+    install_requires=[
+            "sys",
+            "pickle"
+            "numpy==1.19.5",
+            "networkx==2.5",
+            "pandas==1.1.5"
+            ]
+)
