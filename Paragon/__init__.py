8101ba31f1c65715c3163bf68fdb0ebef0ecb6af
mkaanarici <45287457+mkaanarici@users.noreply.github.com>
2023-03-23 12:13:21 +0300

    Update __init__.py

--- Paragon/__init__.py
+++ Paragon/__init__.py
@@ -2,6 +2,7 @@
 from Paragon.GraphletFrequency import GraphletSelect
 from Paragon.Graphlets_lite import GraphletSearch 
 from Paragon.Flux import PageRankFlux   
+from Paragon.NetworkAnalysis import Community_Analysis
 
 def GraphletFrequency(network, Node_list):
     return GraphletSelect(network, Node_list)
@@ -16,3 +17,5 @@ def NetworkInference(network,guide_network=None,edge_attribute=None):
         return PageRankFlux(network=network,motif_network=network,edge_attribute=edge_attribute)
     else:
         return PageRankFlux(network,motif_network=guide_network,edge_attribute=edge_attribute)
+def CommunityAnalysis(subnetwork):
+    return Community_Analysis(subnetwork)
