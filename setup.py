946983c01822e3d4c4eff0207ae57e488c79b25b
mkaanarici <45287457+mkaanarici@users.noreply.github.com>
2023-03-23 12:12:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     url='https://github.com/metunetlab/Paragon',
     python_requires='>=3.6',
     install_requires=[
-            "sys",
-            "pickle"
             "numpy==1.19.5",
             "networkx==2.5",
-            "pandas==1.1.5"
+            "pandas==1.1.5",
+            "community== 1.0.0b1",
+            "scipy==1.5.4",
             ]
 )
